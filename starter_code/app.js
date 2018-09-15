8a1afe3930805dfcc7c0e4251d0479a333394c70
agdwm <almudenaguerraswm@gmail.com>
2018-09-15 11:13:20 +0200

    Modified the access to the beer that return the function random-beers

--- starter_code/app.js
+++ starter_code/app.js
@@ -29,7 +29,7 @@ app.get('/beers', (req, res, next) => {
 app.get('/random-beers', (req, res, next) => {
 	punkAPI.getRandom()
 		.then(beer => {
-			res.render('random-beers', {beer});
+			res.render('random-beers', {beer:beer[0]});
 		})
 		.catch(error => {
 			console.log('ERROR', error)
